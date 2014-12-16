bbddb7c7bbbf4b8319e447f8f05bf517738183d1
Yoshida Toshiyuki <yostos@yostos.org>
2014-12-17 00:11:44 +0900

    Initial Commit

---
+++ SingletonPattern/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SingletonPattern
+//
+//  Created by Yoshida Toshiyuki on 2014/12/17.
+//  Copyright (c) 2014年 Yoshida Toshiyuki. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
